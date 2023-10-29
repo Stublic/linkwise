15887b529ee11ae66e4c8efa43dadc419a7b2edb
stublic.jurica <stublic.jurica@gmail.com>
2023-10-29 20:27:36 +0100

    LinkCustomization

--- components/LinkCustomization.jsx
+++ components/LinkCustomization.jsx
@@ -1,49 +1,73 @@
-'use client';
 import React, { useState, useEffect } from "react";
 import AddLinkForm from "@/components/Addlinkform";
 import Image from "next/image";
 import prisma from "@/lib/prisma";
 
+const LinkCustomization = ({
+  addLinkComponent,
+  showAddLinkForm,
+  linkComponents,
+  addLink,
+  removeLink,
+  handleSubmit,
+}) => {
+  // Declare linkDataFromDatabase in the component's scope
+  const [linkDataFromDatabase, setLinkDataFromDatabase] = useState([]);
 
-const LinkCustomization = ({ addLinkComponent, showAddLinkForm, linkComponents, addLink, removeLink, handleSubmit }) => {
   useEffect(() => {
     // Fetch link data from the database using Prisma
     prisma.link.findMany().then((dbLinks) => {
       if (dbLinks.length > 0) {
         // Create an array of link data for each link retrieved from the database
-        const linkDataFromDatabase = dbLinks.map((link, index) => ({
+        const linkData = dbLinks.map((link) => ({
           platform: link.platform, // Replace with the actual property name in your Prisma model
           link: link.url, // Replace with the actual property name in your Prisma model
         }));
         // Set the link data retrieved from the database
-        // (You can add additional logic here to handle the retrieved data as needed)
+        setLinkDataFromDatabase(linkData);
       }
     });
   }, []);
+
   return (
     <div className="p-6">
       <h2 className="heading-M text-[#333]">Customize your links</h2>
-      <p className="body-M text-[#737373]">Add/edit/remove links below and then share all your profiles with the world!</p>
-      <button onClick={addLinkComponent} className="text-[#C6D752] border-[2px] border-[#C6D752] w-full rounded-lg shadow-sm py-3 my-8 hover:bg-[#E9F0A6] hover:text-[#C6D752]">+ Add new link</button>
-      {(showAddLinkForm || linkDataFromDatabase.length > 0) ? (
+      <p className="body-M text-[#737373]">
+        Add/edit/remove links below and then share all your profiles with the world!
+      </p>
+      <button
+        onClick={addLinkComponent}
+        className="text-[#C6D752] border-[2px] border-[#C6D752] w-full rounded-lg shadow-sm py-3 my-8 hover:bg-[#E9F0A6] hover:text-[#C6D752]"
+      >
+        + Add new link
+      </button>
+      {showAddLinkForm || linkDataFromDatabase.length > 0 ? (
         <div>
           {linkComponents.map((title, index) => (
-           <AddLinkForm
-           onAddLink={addLink}
-           onRemove={removeLink}
-           title={title}
-           key={index}
-           index={index}
-           linkData={linkDataFromDatabase[index]} // Pass the link data retrieved from the database
-           handleSubmit={handleSubmit}
-         />
+            <AddLinkForm
+              onAddLink={addLink}
+              onRemove={removeLink}
+              title={title}
+              key={index}
+              index={index}
+              linkData={linkDataFromDatabase[index]} // Pass the link data retrieved from the database
+              handleSubmit={handleSubmit}
+            />
           ))}
         </div>
       ) : (
         <div className="flex flex-col justify-center items-center">
-          <Image className="mt-6" width={280} height={260} src="/start.svg" alt="start creating links"/>
+          <Image
+            className="mt-6"
+            width={280}
+            height={260}
+            src="/start.svg"
+            alt="start creating links"
+          />
           <h2 className="heading-M my-6 text-center">Let’s get you started</h2>
-          <p className="body-M max-w-lg text-center">Use the “Add new link” button to get started. Once you have more than one link, you can reorder and edit them. We’re here to help you share your profiles with everyone!</p>
+          <p className="body-M max-w-lg text-center">
+            Use the “Add new link” button to get started. Once you have more than one link, you can reorder and edit them. We’re here to help you share your profiles with everyone!
+          </p>
         </div>
       )}
     </div>
