5879640695cdf239ed4809c62031bc15305e215b
stublic.jurica <stublic.jurica@gmail.com>
2023-10-30 00:48:29 +0100

    fixed mobile preview

--- components/Sections.jsx
+++ components/Sections.jsx
@@ -3,8 +3,8 @@ import Links from "@/components/Link";
 
 const LeftSection = ({ links }) => (
   <div className="hidden md:inline-flex h-full min-h-[75vh] w-2/5 p-2 mx-8 my-2 rounded-xl bg-white shadow justify-center items-center gap-8 ">
-    <div className="h-[70vh] flex flex-col justify-between items-center w-80 bg-[url('/mobile.png')] bg-center bg-no-repeat bg-auto">
-      <div className="flex-col justify-start items-center gap-6 flex my-12 pt-2">
+    <div className="h-[80vh] flex flex-col justify-between items-center w-80 bg-[url('/mobile.png')] bg-center bg-no-repeat bg-auto pt-24">
+      <div className="flex-col justify-start items-center gap-6 flex mb-12 pt-2">
         <div className="w-24 h-24 bg-[#EEE] rounded-full" />
         <div className="flex-col justify-start items-center gap-3 flex">
           <div className="w-40 h-4 bg-[#EEE] rounded-full" />
